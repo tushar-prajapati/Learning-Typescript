1ef3d5058f0ffcabc1a51452518aedcac7290664
Tushar Prajapati <tusharprt5@gmail.com>
2025-06-11 12:39:26 +0530

    Type aliases

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,84 @@
 // value = 12;
 // console.log(value);
 
-function addOne(num: number){
-    return num+1;
-}
+// function addOne(num: number){
+//     return num+1;
+// }
 
-const double = (x:number,y:number) => x+y;
+// const double = (x:number,y:number) => x+y;
 
-const result:number =  addOne(3)
-console.log(double(12,3))
+// const result:number =  addOne(3)
+// console.log(double(12,3))
 
-function greet(person = 'anonymous'){
-    return person;
+// function greet(person = 'anonymous'){
+//     return person;
 
-}
+// }
 
-function double1 (x:number):number {
-    return x+1;
-}
-console.log(double1(4));
+// function double1 (x:number):number {
+//     return x+1;
+// }
+// console.log(double1(4));
 
-function printMsg( msg: string):void {
-    console.log(msg)
-}
-printMsg("hello world")
+// function printMsg( msg: string):void {
+//     console.log(msg)
+// }
+// printMsg("hello world")
   
+// function throwError(msg: string): never {
+//     throw new Error(msg);
+// }
+
+// function infiniteLoop ():never{
+//     while(true){}
+// }
+// let x: never;
+// // Arrays:
+// let nums:number[] = [3,2,4,1,3]
+
+// let nums1: number[][]  = [[2,3],[3,5]];
+
+// console.log(nums);
+
+// // Objects
+
+// const person: {firstName: string; lastName: string, age: number} = {
+//     firstName : "John",
+//     lastName: "Doe",
+//     age: 22,
+// }
+
+// console.log(person.firstName+" "+person.lastName)
+
+// // Using object as function return value;
+
+// const f1 = ():{name: string, age: number}=>{
+//     return {
+//         name: "Tushar",
+//         age: 21
+//     }
+// }
+
+// const person = f1();
+
+// console.log(person.name);
+
+// // Type alias
+
+// type Person = {
+//     name: string,
+//     age: number
+// }
+
+// const person: Person = {
+//     name: "John",
+//     age: 22
+// }
+
+// console.log(`Name: ${person.name} \n Age: ${person.age}`)
+
+
+
+
+
+
