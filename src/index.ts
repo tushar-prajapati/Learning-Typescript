e1e387bea5df672657638b9fb7f362c6a04114f6
Tushar Prajapati <tusharprt5@gmail.com>
2025-06-11 11:24:25 +0530

    setup

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+// let value: any = "Hello";
+// value = 12;
+// console.log(value);
+
+function addOne(num: number){
+    return num+1;
+}
+
+const double = (x:number,y:number) => x+y;
+
+const result:number =  addOne(3)
+console.log(double(12,3))
+
+function greet(person = 'anonymous'){
+    return person;
+
+}
+
+function double1 (x:number):number {
+    return x+1;
+}
+console.log(double1(4));
+
+function printMsg( msg: string):void {
+    console.log(msg)
+}
+printMsg("hello world")
+
